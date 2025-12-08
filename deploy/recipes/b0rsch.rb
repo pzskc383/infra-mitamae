27b992c83e5501ec3895a756b59fd2ce4fa9122a
Alexander D. <maniac@pzskc383.net>
2025-12-09 00:47:19 +0200

    pf refactor

--- deploy/recipes/b0rsch.rb
+++ deploy/recipes/b0rsch.rb
@@ -1,5 +1,5 @@
 include_recipe "cookbooks/openbsd_server/default.rb"
-include_recipe "cookbooks/pf/dynamic.rb"
+include_recipe "cookbooks/pf/defines.rb"
 include_recipe "cookbooks/openbsd_com0/default.rb"
 include_recipe "cookbooks/openbsd_admin/default.rb"
 include_recipe "cookbooks/pf/default.rb"
