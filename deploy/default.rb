0e246fbb8bd01e57844e9976d7d37ef2c405eebf
Alexander D. <maniac@pzskc383.net>
2025-12-12 14:47:33 +0200

    Refactored configs, improved service management.
    (WIP, w/e)

--- deploy/default.rb
+++ deploy/default.rb
@@ -1,19 +1,7 @@
 include_recipe 'defines.rb'
-host "airstrip1" do
-  attributes(
-    dns_shortname: 'a1'
-  )
-end
-
 host "a2mm" do
   attributes(
     dns_shortname: 'a2'
-  )
-end
-
-host "b0rsch" do
-  attributes(
-    dns_shortname: 'b0'
   )
 end
 
@@ -23,15 +11,9 @@ host "f0rk" do
   )
 end
 
-# run_on "airstrip1" do
-#   file "deploy/recipes/airstrip1.rb"
-# end
 # run_on "a2mm" do
 #   file "deploy/recipes/a2mm.rb"
 # end
-run_on "b0rsch" do
-  file "deploy/recipes/b0rsch.rb"
-end
 # run_on "f0rk" do
 # file "deploy/recipes/f0rk.rb"
 # end
